e32582f99f2532e348e4e99dae67df238e57ea21
Rudesh <rudeshdhote@gmail.com>
2024-09-08 00:24:36 +0200

    recode the project

--- src/app/api/dataPoint/route.ts
+++ src/app/api/dataPoint/route.ts
@@ -1,14 +1,23 @@
 
 import { query } from "@/utils/dbs";
+import { NextResponse } from "next/server";
 
 export async function GET() {
-    const users = await query({
-        query: "SELECT * FROM envotech.DataPartition",
+    try {
+      // Query to fetch data from `envotech.DataPartition`
+      const users = await query({
+        query: 'SELECT * FROM envotech.DataPartition', // Same key `query` for the query string
         values: [],
-    });
-
-    let data = JSON.stringify(users);
-    return new Response(data, {
-        status: 200,
-    });
-}
+      });
+  
+      // Convert the result to JSON
+      const data = JSON.stringify(users);
+      console.log({ data });
+  
+      // Return the response with the data
+      return NextResponse.json(data, { status: 200 });
+    } catch (error) {
+      console.error('Failed to fetch users:', error);
+      return NextResponse.json({ message: 'Failed to fetch users' }, { status: 500 });
+    }
+  }
