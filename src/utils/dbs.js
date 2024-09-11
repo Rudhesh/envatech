bf7032a3289ddd8acf6f19c35b1a6673237a2424
Rudesh <rudeshdhote@gmail.com>
2024-09-11 21:03:14 +0200

    test search component

--- src/utils/dbs.js
+++ src/utils/dbs.js
@@ -7,6 +7,7 @@ export async function query({ query, values = [] }) {
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
+        port: process.env.DB_PORT || 3306,
       });
 
   try {
